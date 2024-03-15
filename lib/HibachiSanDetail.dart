32a4e651bdc4590e5aa572c07e8bcc3830d31b81
cchua00 <97256479+cchua00@users.noreply.github.com>
2024-03-15 04:24:50 -0700

    Update HibachiSanDetail.dart

--- lib/HibachiSanDetail.dart
+++ lib/HibachiSanDetail.dart
@@ -97,13 +97,9 @@ class _HibachiSanDetailState extends State<HibachiSanDetail> with SingleTickerPr
             labelColor: Colors.white,
             unselectedLabelColor: Colors.white,
             tabs: [
-              Tab(text: 'Teppanyaki'),
-              Tab(text: 'Sushi'),
-              Tab(text: 'Appetizers'),
-              Tab(text: 'Beverages'),
-              Tab(text: 'Desserts'),
-              Tab(text: 'Combos'),
-              Tab(text: 'Specials'),
+              Tab(text: 'Poke Bowls'),
+              Tab(text: 'Tea Bar Drinks'),
+
             ],
           ).preferredSize,
           child: Align(
@@ -115,13 +111,9 @@ class _HibachiSanDetailState extends State<HibachiSanDetail> with SingleTickerPr
               unselectedLabelColor: Colors.white,
               indicatorSize: TabBarIndicatorSize.tab,
               tabs: [
-                Tab(text: 'Teppanyaki'),
-                Tab(text: 'Sushi'),
-                Tab(text: 'Appetizers'),
-                Tab(text: 'Beverages'),
-                Tab(text: 'Desserts'),
-                Tab(text: 'Combos'),
-                Tab(text: 'Specials'),
+                Tab(text: 'Poke Bowls'),
+                Tab(text: 'Tea Bar Drinks'),
+
               ],
             ),
           ),
@@ -136,28 +128,198 @@ class _HibachiSanDetailState extends State<HibachiSanDetail> with SingleTickerPr
             index: 0,
             child: FoodCategory(
               key: _keys[0],
-              categoryName: 'Teppanyaki',
+              categoryName: 'Poke Bowls',
               isFirstCategory: true,
               foodList: [
                 // Example FoodItem, repeat structure for other menu items
                 FoodItem(
-                  name: 'Chicken Teppanyaki',
-                  description: 'Grilled chicken with vegetables and your choice of rice or noodles',
-                  image: 'assets/images/hibachi_san/teppanyaki.jpg',
-                  price: 12.99,
+                  name: 'Small',
+                  description: '2 proteins served with rice/salad and toppings',
+                  image: 'assets/images/hibachi/Small.jpg',
+                  price: 10.50,
                   requiredOptions: [
                     RequiredOption(
-                        name: "Side",
-                        options: ["White Rice", "Brown Rice", "Fried Rice", "Noodles"]
+                        name: "Base",
+                        options: ["White Rice", "Brown Rice", "Mixed Salad", "1/2 and 1/2"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 1",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 2",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Toppings",
+                        options: ["Corn Salad", "Fried Onion", "Avocado", "Ginger", "Seaweed Salad"]
                     ),
                     RequiredOption(
                         name: "Sauce",
-                        options: ["Teriyaki", "Soy Sauce", "Spicy Mayo"]
+                        options: ["Teriyaki", "Spicy Mayo", "Ponzu"]
                     ),
                   ],
                   extras: [
-                    ExtraOption(name: "Extra Chicken", price: 3.00),
-                    ExtraOption(name: "Add Shrimp", price: 4.00),
+                    ExtraOption(name: "Extra Protein", price: 2.00),
+                    ExtraOption(name: "Add Corn Salad", price: 0.00),
+                    ExtraOption(name: "Add Fried Onion", price: 0.00),
+                    ExtraOption(name: "Add Avocado", price: 0.00),
+                    ExtraOption(name: "Add Ginger", price: 0.00),
+                    ExtraOption(name: "Add Seaweed", price: 0.00),
+                  ],
+                ),
+                FoodItem(
+                  name: 'Regular',
+                  description: '3 proteins served with rice/salad and toppings',
+                  image: 'assets/images/hibachi/Regular.jpg',
+                  price: 12.00,
+                  requiredOptions: [
+                    RequiredOption(
+                        name: "Base",
+                        options: ["White Rice", "Brown Rice", "Mixed Salad", "1/2 and 1/2"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 1",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 2",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 3",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Toppings",
+                        options: ["Corn Salad", "Fried Onion", "Avocado", "Ginger", "Seaweed Salad"]
+                    ),
+                    RequiredOption(
+                        name: "Sauce",
+                        options: ["Teriyaki", "Spicy Mayo", "Ponzu"]
+                    ),
+                  ],
+                  extras: [
+                    ExtraOption(name: "Extra Protein", price: 2.00),
+                    ExtraOption(name: "Add Corn Salad", price: 0.00),
+                    ExtraOption(name: "Add Fried Onion", price: 0.00),
+                    ExtraOption(name: "Add Avocado", price: 0.00),
+                    ExtraOption(name: "Add Ginger", price: 0.00),
+                    ExtraOption(name: "Add Seaweed", price: 0.00),
+                  ],
+                ),
+                FoodItem(
+                  name: 'Large',
+                  description: '4 proteins served with rice/salad and toppings',
+                  image: 'assets/images/hibachi/Large.jpg',
+                  price: 13.50,
+                  requiredOptions: [
+                    RequiredOption(
+                        name: "Base",
+                        options: ["White Rice", "Brown Rice", "Mixed Salad", "1/2 and 1/2"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 1",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 2",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 3",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Protein 4",
+                        options: ["Grilled Chicken", "Tri-Tip Beef", "Spam", "Original Ahi Tuna", "Sriracha Ahi Tuna", "Original Salmon", "Spicy Yuzu Salmon", "Creamy Bay Scallops, Crab Mix" , "Spicy Crab mix", "Tofu"]
+                    ),
+                    RequiredOption(
+                        name: "Toppings",
+                        options: ["Corn Salad", "Fried Onion", "Avocado", "Ginger", "Seaweed Salad"]
+                    ),
+                    RequiredOption(
+                        name: "Sauce",
+                        options: ["Teriyaki", "Spicy Mayo", "Ponzu"]
+                    ),
+                  ],
+                  extras: [
+                    ExtraOption(name: "Extra Protein", price: 2.00),
+                    ExtraOption(name: "Add Corn Salad", price: 0.00),
+                    ExtraOption(name: "Add Fried Onion", price: 0.00),
+                    ExtraOption(name: "Add Avocado", price: 0.00),
+                    ExtraOption(name: "Add Ginger", price: 0.00),
+                    ExtraOption(name: "Add Seaweed", price: 0.00),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          AutoScrollTag(
+            key: _keys[1],
+            controller: _scrollController,
+            index: 1,
+            child: FoodCategory(
+              key: _keys[1],
+              categoryName: 'Tea Bar Drinks',
+              isFirstCategory: true,
+              foodList: [
+                // Example FoodItem, repeat structure for other menu items
+                FoodItem(
+                  name: 'Thai Milk Tea',
+                  description: 'A fragrant and creamy delight that combines black tea, spices, condense milk.',
+                  image: 'assets/images/hibachi/ThaiTea.jpg',
+                  price: 3.75,
+                  requiredOptions: [
+                    RequiredOption(
+                        name: "Size",
+                        options: ["Cup", "Bottle" ],
+                        optionPrices: {
+                            "Cup": 0.00,
+                            "Bottle": 0.50
+                        }
+                    ),
+                  ],
+                  extras: [
+                    ExtraOption(name: "Add boba?", price: 0.50),
+                  ],
+                ),
+                FoodItem(
+                  name: 'Earl Grey Milk Tea',
+                  description: 'A fragrant and creamy delight that combines earl black tea, spices, condense milk.',
+                  image: 'assets/images/hibachi/EarlGrey.jpg',
+                  price: 3.75,
+                  requiredOptions: [
+                    RequiredOption(
+                        name: "Size",
+                        options: ["Cup", "Bottle" ],
+                        optionPrices: {
+                          "Cup": 0.00,
+                          "Bottle": 0.50
+                        }
+                    ),
+                  ],
+                  extras: [
+                    ExtraOption(name: "Add boba?", price: 0.50),
+                  ],
+                ),
+                FoodItem(
+                  name: 'Passion Fruit Mango Black Tea',
+                  description: 'A fragrant and fruit delight that combines black tea, fruit, condense milk.',
+                  image: 'assets/images/hibachi/PassionFruit.jpg',
+                  price: 3.75,
+                  requiredOptions: [
+                    RequiredOption(
+                        name: "Size",
+                        options: ["Cup", "Bottle" ],
+                        optionPrices: {
+                          "Cup": 0.00,
+                          "Bottle": 0.50
+                        }
+                    ),
+                  ],
+                  extras: [
+                    ExtraOption(name: "Add boba?", price: 0.50),
                   ],
                 ),
               ],
